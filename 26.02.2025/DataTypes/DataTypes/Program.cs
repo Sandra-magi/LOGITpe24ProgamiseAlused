bff84761b09972b535024a993d781f7f8c625fbc
Fenkii3 <kevin.allik08@gmail.com>
2025-02-26 11:38:07 +0200

    dadada

---
+++ 26.02.2025/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data types!");
+
+            //täisnumber
+            int bigNumber = 1234567890;
+            Console.WriteLine(bigNumber);
+
+            //komaga number ??
+            double comanumber = 12.5;
+            Console.WriteLine(comanumber);
+
+            //tähemärk
+            string stringVar = "asd456$%&*(~~~~~~84u5098lnfigu+-";
+
+            float floatVariable = 123.123f;
+            Console.WriteLine(floatVariable);
+
+            char charVariable = '2';
+            Console.WriteLine(charVariable);
+
+            bool boolVariable = false;
+            Console.WriteLine(boolVariable);
+        }
+    }
+}
