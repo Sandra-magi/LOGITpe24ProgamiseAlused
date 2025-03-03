0fb04689279d372814d361c1f84e61f6ff3f1287
Fenkii3 <kevin.allik08@gmail.com>
2025-03-03 19:36:22 +0200

    Kalkulaator

---
+++ 02.03.25/Kalkulaator/Kalkulaator/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Sisesta esimene arv:");
+double arv1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Sisesta tehe (+, -, *, /):");
+char tehe = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("Sisesta teine arv:");
+double arv2 = Convert.ToDouble(Console.ReadLine());
+
+double tulemus = 0;
+
+if (tehe == '+')
+{
+    tulemus = arv1 + arv2;
+}
+else if (tehe == '-')
+{
+    tulemus = arv1 - arv2;
+}
+else if (tehe == '*')
+{
+    tulemus = arv1 * arv2;
+}
+else if (tehe == '/')
+{
+    if (arv2 != 0)
+    {
+        tulemus = arv1 / arv2;
+    }
+    else
+    {
+        Console.WriteLine("Viga! Nulliga jagamine pole lubatud.");
+        return;
+    }
+}
+else
+{
+    Console.WriteLine("Viga! Vigane tehe.");
+    return;
+}
+
+Console.WriteLine("Tulemus: " + tulemus);
+
+
