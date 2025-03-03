857f267f283c160219d92c7e58b5a6aac986fcec
Opilane TTHK <opilane@tthk.ee>
2025-03-03 15:03:41 +0200

    Ümbermaa

---
+++ 03.03.25/Ümbermaa/Ümbermaa/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ümbermaa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //vaja arvutada, mitu kahe eurost münti läheb ümber maa
+            //maa ümbermõõt tuleb välja arvutada raadiuse abil
+
+            double coinDiameter = 25.75;
+            double earthRadius = 6378000000;
+
+            //arvutan ümbermõõdu
+            double circumference = 2 * Math.PI * earthRadius;
+
+            //arvutan mitu münti läheb ümber maa
+            double numberOfCoins = circumference / coinDiameter;
+
+            Console.WriteLine("Mitu 2 eurost münti läheb ümber maa: " + Math.Round(numberOfCoins));
+
+        }
+    }
+}
